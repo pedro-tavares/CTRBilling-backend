c84aa319911211426e02f66b93ee40b936d8c9d3
Pedro Alexandre Tavares <patavares@outlook.com>
2020-04-18 11:21:10 +0200

    Add to Person

---
+++ src/main/resources/sql/Add_Person.sql
@@ -0,0 +1,4 @@
+INSERT INTO person (id, first_name, email, the_password, roles, enabled) VALUES(1, 'Superman', 'superman@mail', 'FRvZWXRhxPPC8TNEjIGxsUjjSEEpDn1zZhVCz99Uq0U=', 'master,reseller,customer', true);
+INSERT INTO person (id, first_name, email, the_password, roles, enabled) VALUES(2, 'Reseller 1', 'reseller1@mail', 'FRvZWXRhxPPC8TNEjIGxsUjjSEEpDn1zZhVCz99Uq0U=', 'reseller', true);
+INSERT INTO person (id, first_name, email, the_password, roles, enabled) VALUES(3, 'Customer 1', 'customer1@mail', 'FRvZWXRhxPPC8TNEjIGxsUjjSEEpDn1zZhVCz99Uq0U=', 'customer', true);
+
