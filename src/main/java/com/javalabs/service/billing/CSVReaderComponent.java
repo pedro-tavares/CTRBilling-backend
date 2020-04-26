af39264fa0686aca19505d631e4e77192e11701a
Pedro Alexandre Tavares <patavares@outlook.com>
2020-04-26 07:58:52 +0000

    New CSVReaderComponent

---
+++ src/main/java/com/javalabs/service/billing/CSVReaderComponent.java
@@ -0,0 +1,43 @@
+package com.javalabs.service.billing;
+
+import com.javalabs.dto.DowloadFTPFileInfo;
+import com.opencsv.CSVReader;
+
+import java.io.FileReader;
+import java.io.IOException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CSVReaderComponent {
+	
+	private static Logger LOG = LoggerFactory.getLogger(CSVReaderComponent.class);
+
+	public boolean process(DowloadFTPFileInfo fileInfo) {
+
+        String csvFile = fileInfo.getFileName();
+        
+		LOG.debug(
+				"\nCSVReaderComponent PROCESS, server:" + fileInfo.getServer().getName() 
+				+ ", fileName:" + fileInfo.getFileName()
+		);        
+
+        CSVReader reader = null;
+        try {
+            reader = new CSVReader(new FileReader(csvFile));
+            String[] line;
+            while ((line = reader.readNext()) != null) {
+                LOG.debug("BillingRecord [" + 
+                		"Call Type=" + line[0] + 
+                		", Call Cause=" + line[1] + 
+                		", Customer Identifier=" + line[2] + "]"
+                );
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return true;
+    }
+
+}
