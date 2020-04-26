1c8117a1e3860aa7682cac13fc701a4ca4eadfb5
Pedro Alexandre Tavares <patavares@outlook.com>
2020-04-26 03:01:45 +0000

    Controller to Service mapping

--- src/main/java/com/javalabs/controller/BillingController.java
+++ src/main/java/com/javalabs/controller/BillingController.java
@@ -4,10 +4,15 @@ import org.dozer.DozerBeanMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.javalabs.dto.DowloadFTPFileInfo;
 import com.javalabs.service.billing.BillingService;
 
 @RestController
@@ -23,4 +28,20 @@ public class BillingController {
 	@Autowired
 	DozerBeanMapper mapper;
 
+	@RequestMapping(path = "/process_file", method = RequestMethod.POST)
+	public ResponseEntity<String> processFile(@RequestBody DowloadFTPFileInfo fileInfo) throws Exception {
+		LOG.debug(
+				"\nBillingController PROCESS_FILE, server:" + fileInfo.getServer().getName() 
+				+ ", user:" + fileInfo.getServer().getUsername()
+				+ ", fileName:" + fileInfo.getFileName()
+		);
+
+		if (billingService.process(fileInfo)) {
+			return ResponseEntity.ok().build();
+		}
+		
+		return ResponseEntity.ok().build();
+		//return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+	}
+	
 }
--- src/main/java/com/javalabs/service/billing/BillingService.java
+++ src/main/java/com/javalabs/service/billing/BillingService.java
@@ -4,9 +4,15 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
+import com.javalabs.dto.DowloadFTPFileInfo;
+
 @Service
 public class BillingService {
 	
 	private static Logger LOG = LoggerFactory.getLogger(BillingService.class);
+
+	public boolean process(DowloadFTPFileInfo fileInfo) throws Exception {
+		return true;
+	}
 
 }
