f1a004c32c7cf9c760b3bba6746e13b9d91b933c
Pedro Alexandre Tavares <patavares@outlook.com>
2020-04-26 14:35:17 +0000

    Update CSV Reader

--- src/main/java/com/javalabs/service/billing/CSVReaderComponent.java
+++ src/main/java/com/javalabs/service/billing/CSVReaderComponent.java
@@ -33,7 +33,47 @@ public class CSVReaderComponent {
                 LOG.debug("BillingRecord [" + 
                 		"Call Type=" + line[0] + 
                 		", Call Cause=" + line[1] + 
-                		", Customer Identifier=" + line[2] + "]"
+                		", Customer Identifier=" + line[2] +
+                		", Telephone Number Dialled=" + line[3] +
+                		", Call Date=" + line[4] +
+                		", Call Time=" + line[5] +
+                		", Duration=" + line[6] +
+                		", Bytes Transmitted=" + line[7] +
+                		", Bytes Received=" + line[8] +
+                		", Description=" + line[9] +
+                		", Chargecode=" + line[10] +
+                		", Time Band=" + line[11] +
+                		", Salesprice=" + line[12] +
+                		", Salesprice (pre-bundle)=" + line[13] +
+                		", Extension=" + line[14] +
+                		", DDI=" + line[15] +
+                		", Grouping ID=" + line[16] +
+                		", Call Class=" + line[17] +
+                		", Carrier=" + line[18] +
+                		", Recording=" + line[19] +
+                		", VAT=" + line[20] +
+                		", Country of Origin=" + line[21] +
+                		", Network=" + line[22] +
+                		", Retail tariff code=" + line[23] +
+                		", Remote Network=" + line[24] +
+                		", APN=" + line[25] +
+                		", Diverted Number=" + line[26] +
+                		", Ring time=" + line[27] +
+                		", RecordID=" + line[28] +
+                		", Currency=" + line[29] +
+                		", Presentation Number=" + line[30] +
+                		", Network Access Reference=" + line[31] +
+                		", NGCS Access Charge=" + line[32] +
+                		", NGCS Service Charge=" + line[33] +
+                		", Total Bytes Transferred=" + line[34] +
+                		", User ID=" + line[35] +
+                		", Onward Billing Reference=" + line[36] +
+                		", Contract Name=" + line[37] +
+                		", Bundle Name=" + line[38] +
+                		", Bundle Allowance=" + line[39] +
+                		", Discount Reference=" + line[40] +
+                		", Routing Code=" + line[41] +
+                		"]"
                 );
             }
         } catch (IOException e) {
